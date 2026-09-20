9d1fd2ab98f31b80768e5b66c5f6b82d63e506de
ScaleUPPeisov <peisov.business@gmail.com>
2026-09-20 16:59:06 +0700

    fix: throttle background linked-channel statistics refresh

--- src/ChannelStatisticsScheduler.tsx
+++ src/ChannelStatisticsScheduler.tsx
@@ -1,13 +1,12 @@
 import {useEffect} from 'react';
-import {CHANNEL_STATS_TTL_MS} from './youtubeChannelStats';
-import {refreshYoutubeChannelStatistics} from './youtubeChannelStatsRuntime';
+import {BACKGROUND_CHANNEL_STATS_TTL_MS,refreshYoutubeChannelStatistics} from './youtubeChannelStatsRuntime';
 
 export function ChannelStatisticsScheduler(){
   useEffect(()=>{
     let cancelled=false;
     const run=()=>{if(!cancelled)void refreshYoutubeChannelStatistics(false).catch(()=>{})};
     run();
-    const id=window.setInterval(run,CHANNEL_STATS_TTL_MS);
+    const id=window.setInterval(run,BACKGROUND_CHANNEL_STATS_TTL_MS);
     return()=>{cancelled=true;window.clearInterval(id)};
   },[]);
   return null;
