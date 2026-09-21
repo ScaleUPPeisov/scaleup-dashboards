f9d8e73bcca56fb99dc6dac54cf75672fca0d123
ScaleUPPeisov <peisov.business@gmail.com>
2026-09-21 22:15:42 +0700

    build: embed unique VYRON build revision and channel

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,1 +1,9 @@
-fn main(){tauri_build::build()}
+fn main(){
+    let revision=std::env::var("VYRON_BUILD_REVISION").unwrap_or_else(|_|"0".into());
+    let commit=std::env::var("VYRON_COMMIT_SHA").unwrap_or_else(|_|"development".into());
+    let channel=std::env::var("VYRON_UPDATE_CHANNEL").unwrap_or_else(|_|"stable".into());
+    println!("cargo:rustc-env=VYRON_BUILD_REVISION={revision}");
+    println!("cargo:rustc-env=VYRON_COMMIT_SHA={commit}");
+    println!("cargo:rustc-env=VYRON_UPDATE_CHANNEL={channel}");
+    tauri_build::build()
+}
