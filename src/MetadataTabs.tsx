a8c72f122fbd4b629982cdbff202bdf2d2e39e52
ScaleUPPeisov <peisov.business@gmail.com>
2026-09-22 20:55:57 +0700

    refactor(vyron): remove Shorts metadata tab

--- src/MetadataTabs.tsx
+++ src/MetadataTabs.tsx
@@ -1,4 +1,3 @@
-import React,{useState} from 'react';
+import React from 'react';
 import {MetadataPage} from './MetadataPage';
-import {ShortsMetadata} from './ShortsMetadata';
-export function MetadataTabs(){const [tab,setTab]=useState<'video'|'shorts'>('video');return <><div className="youtubeTabs metadataTypeTabs"><button className={tab==='video'?'active':''} onClick={()=>setTab('video')}>VIDEO</button><button className={tab==='shorts'?'active':''} onClick={()=>setTab('shorts')}>SHORTS</button></div>{tab==='video'?<MetadataPage/>:<ShortsMetadata/>}</>}
+export function MetadataTabs(){return <MetadataPage/>}
